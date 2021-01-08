a7955f5b227a332221e18b01e270da4d47ebb186
Alan-Grech <39163868+Alan-Grech@users.noreply.github.com>
2021-01-08 17:22:12 +0100

    Added logger.

--- Proton/src/Proton.h
+++ Proton/src/Proton.h
@@ -3,6 +3,7 @@
 //For use by Proton Applications
 
 #include "Proton/Application.h"
+#include "Proton/Log.h"
 
 //---Entry Point------------------
 #include "Proton/EntryPoint.h"
---
+++ Proton/src/Proton/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Proton
+{
+	class PROTON_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//Core Log Macros
+#define PT_CORE_TRACE(...)     ::Proton::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define PT_CORE_INFO(...)      ::Proton::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define PT_CORE_WARN(...)      ::Proton::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define PT_CORE_ERROR(...)     ::Proton::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define PT_CORE_FATAL(...)     ::Proton::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//Client Log Macros
+#define PT_TRACE(...)          ::Proton::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define PT_INFO(...)           ::Proton::Log::GetClientLogger()->info(__VA_ARGS__)
+#define PT_WARN(...)           ::Proton::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define PT_ERROR(...)          ::Proton::Log::GetClientLogger()->error(__VA_ARGS__)
+#define PT_FATAL(...)          ::Proton::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
