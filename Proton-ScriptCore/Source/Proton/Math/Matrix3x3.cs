e26903e875f2a6b8fbb022f41042ed43216d8577
Alan-Grech295 <77152357+Alan-Grech295@users.noreply.github.com>
2023-03-02 16:07:13 +0100

    Updates

---
+++ Proton-ScriptCore/Source/Proton/Math/Matrix3x3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Proton.Math
+{
+    public class Matrix3x3
+    {
+        public float[,] matrix = new float[3,3];
+
+        internal Matrix3x3(float[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+    }
+}
